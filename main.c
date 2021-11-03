23c27eb01a91940bc430a807fb7a792184eea213
Noah Chernet <noahchernet@yahoo.com>
2021-11-03 17:12:32 +0300

    Cleaned up old code

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#define TOKEN_LEN 16
 
 /**
  * main - simple shell
@@ -49,78 +48,4 @@ int main(void)
 
 		wait(&current_status);
 	}
-}
-/**
- * tokenizer - splits str into tokens
- * @str: string to be tokenzed
- * Return: array of token strings
- */
-char **tokenizer(char *str)
-{
-	char **tokens = NULL, *token;
-	const char *DELIM = " \t\a\r";
-	size_t position = 0, tok_size = TOKEN_LEN, i;
-
-	tokens = malloc(tok_size * sizeof(char *));
-	if (tokens == NULL)
-		return (NULL);
-
-	token = strtok(str, DELIM);
-	while (token != NULL)
-	{
-		tokens[position++] = _strdup(token);
-
-		/* If allocated memory isn't enough */
-		if (position > tok_size)
-		{
-			tok_size += TOKEN_LEN;
-			tokens = _realloc(tokens, position + 1, tok_size);
-			if (tokens == NULL)
-			{
-				for (i = 0; i < position; i++)
-					free(tokens[i]);
-				return (NULL);
-			}
-		}
-		token = strtok(NULL, DELIM);
-	}
-	tokens[position] = NULL;
-
-	return (tokens);
-}
-
-/**
- * launch - gets path for str
- * @str: path key
- * Return: status
- */
-int launch(char **str)
-{
-	int status = 1;
-	char *path, *tok, *cmd;
-	struct stat statbuf;
-
-	/* get path */
-	path = _getenv("PATH");
-
-	/* find given string */
-	tok = strtok(path, ":");
-	while (tok != NULL)
-	{
-		/* find if the commmand exists in a path */
-		cmd = _strdup(tok);
-		_strcat(_strcat(cmd, "/"), *str);
-		if (stat(cmd, &statbuf) == 0)
-		{
-			free(*str);
-			*str = _strdup(cmd);
-			status = 0;
-			break;
-		}
-		free(cmd);
-		tok = strtok(NULL, ":");
-	}
-	free(path);
-
-	return (status);
 }
