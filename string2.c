a337ab6815d6b811069feb4d041055d9d2632f70
Noah Chernet <noahchernet@yahoo.com>
2021-11-03 21:28:05 +0300

    Added new files

--- string2.c
+++ string2.c
@@ -3,35 +3,6 @@
 #include "main.h"
 
 /**
- * _atoi - convert string to an integer
- * @s: string
- * Return: integer
- */
-int _atoi(char *s)
-{
-	int i = 0, sign = 1, val = 0;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			if (val > INT_MAX / 10 || (val == INT_MAX / 10))
-			{
-				return (sign == -1 ? INT_MIN : INT_MAX);
-			}
-			val = val * 10 + s[i] - '0';
-		}
-		else if (val != 0)
-			break;
-		i++;
-	}
-
-	return (sign * val);
-}
-
-/**
  * print_num - prints an int
  * @n: int to print
  *
@@ -72,3 +43,4 @@ void print(char *s)
 {
 	write(STDOUT_FILENO, s, _strlen(s));
 }
+
