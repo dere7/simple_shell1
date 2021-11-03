3b53c2899420384134f0ac5c194506b102a10b36
Noah Chernet <noahchernet@yahoo.com>
2021-11-03 21:36:10 +0300

    Revert "Revert "Added new files""
    This reverts commit 47af2961ca9adc1d7ab541fcea88c1aa768b701a.

--- builtins.c
+++ builtins.c
@@ -1,116 +1,157 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
 #include "main.h"
 
 /**
- * cd - change current working directory
- * @args: array of strings
- * Return: status
+ * check_for_builtins - checks if the command is a builtin
+ * @vars: variables
+ * Return: pointer to the function or NULL
  */
-int cd(char **args)
+void (*check_for_builtins(vars_t *vars))(vars_t *vars)
+{
+	unsigned int i;
+	builtins_t check[] = {
+		{"exit", new_exit},
+		{"env", _env},
+		{"setenv", new_setenv},
+		{"unsetenv", new_unsetenv},
+		{NULL, NULL}
+	};
+
+	for (i = 0; check[i].f != NULL; i++)
+	{
+		if (_strcmpr(vars->av[0], check[i].name) == 0)
+			break;
+	}
+	if (check[i].f != NULL)
+		check[i].f(vars);
+	return (check[i].f);
+}
+
+/**
+ * new_exit - exit program
+ * @vars: variables
+ * Return: void
+ */
+void new_exit(vars_t *vars)
 {
 	int status;
-	char *pwd = NULL;
 
-	if (args[1] == NULL)
+	if (_strcmpr(vars->av[0], "exit") == 0 && vars->av[1] != NULL)
 	{
-		pwd = _getenv("HOME");
-		status = chdir(pwd);
-	}
-	else if (_strcmp(args[1], "-") == 0)
-	{
-		pwd = _getenv("OLDPWD");
-		status = chdir(pwd);
-	}
-	else
-		status = chdir(args[1]);
-
-	return (status);
-}
-/**
- * _getenv - get an environment variable
- * @name: enviroment variable key
- * Return: status
- */
-char *_getenv(char *name)
-{
-	int i;
-	char *path;
-
-	for (i = 0; __environ[i] != NULL; i++)
-	{
-		if (_strstr(__environ[i], name) != NULL)
+		status = _atoi(vars->av[1]);
+		if (status == -1)
 		{
-			path = _strdup(__environ[i] + strlen(name) + 1);
-			break;
+			vars->status = 2;
+			print_error(vars, ": Illegal number: ");
+			_puts2(vars->av[1]);
+			_puts2("\n");
+			free(vars->commands);
+			vars->commands = NULL;
+			return;
 		}
+		vars->status = status;
 	}
-
-	if (__environ[i] == NULL)
-		return (NULL);
-	return (path);
+	free(vars->buffer);
+	free(vars->av);
+	free(vars->commands);
+	free_env(vars->env);
+	exit(vars->status);
 }
-/**
- * help - print help
- * @args: array of strings
- * Return: status
- */
-int help(char **args, builtin_t *builtins)
-{
-	int i;
 
-	if (args[1] != NULL)
+/**
+ * _env - prints the current environment
+ * @vars: struct of variables
+ * Return: void.
+ */
+void _env(vars_t *vars)
+{
+	unsigned int i;
+
+	for (i = 0; vars->env[i]; i++)
 	{
-		for (i = 0; builtins[i].name != NULL; i++)
-			if (_strcmp(args[1], builtins[i].name) == 0)
-			{
-				printBuiltins(builtins[i]);
-				break;
-			}
-		if (builtins[i].name == NULL)
+		_puts(vars->env[i]);
+		_puts("\n");
+	}
+	vars->status = 0;
+}
+
+/**
+ * new_setenv - create a new environment variable, or edit an existing variable
+ * @vars: pointer to struct of variables
+ *
+ * Return: void
+ */
+void new_setenv(vars_t *vars)
+{
+	char **key;
+	char *var;
+
+	if (vars->av[1] == NULL || vars->av[2] == NULL)
+	{
+		print_error(vars, ": Incorrect number of arguments\n");
+		vars->status = 2;
+		return;
+	}
+	key = find_key(vars->env, vars->av[1]);
+	if (key == NULL)
+		add_key(vars);
+	else
+	{
+		var = add_value(vars->av[1], vars->av[2]);
+		if (var == NULL)
 		{
-			print(args[1]);
-			print(" not a builtin command\n");
-			return (1);
+			print_error(vars, NULL);
+			free(vars->buffer);
+			free(vars->commands);
+			free(vars->av);
+			free_env(vars->env);
+			exit(127);
 		}
+		free(*key);
+		*key = var;
 	}
-	else
-	{
-		print("Simple Shell\nThe following are built in:\n");
-		for (i = 0; builtins[i].name != NULL; i++)
-			printBuiltins(builtins[i]);
-		print("Use man for other commands\n");
-	}
-	return (0);
+	vars->status = 0;
 }
-/**
- * cexit - exits
- * @args: array of strings
- * Return: status
- */
-int cexit(char **args)
-{
-	if (args[1] == NULL)
-		_exit(errno);
-	else
-		_exit(_atoi(args[1]));
-}
-/**
- * env - prints enviromental variables
- * @args: array of strings
- * Return: status
- */
-int env(char **args __attribute__((unused)))
-{
-	int i;
 
-	for (i = 0; __environ[i]; i++)
+/**
+ * new_unsetenv - remove an environment variable
+ * @vars: pointer to a struct of variables
+ *
+ * Return: void
+ */
+void new_unsetenv(vars_t *vars)
+{
+	char **key, **newenv;
+
+	unsigned int i, j;
+
+	if (vars->av[1] == NULL)
 	{
-		print(__environ[i]);
-		_putchar('\n');
+		print_error(vars, ": Incorrect number of arguments\n");
+		vars->status = 2;
+		return;
 	}
-
-	return (0);
+	key = find_key(vars->env, vars->av[1]);
+	if (key == NULL)
+	{
+		print_error(vars, ": No variable to unset");
+		return;
+	}
+	for (i = 0; vars->env[i] != NULL; i++)
+		;
+	newenv = malloc(sizeof(char *) * i);
+	if (newenv == NULL)
+	{
+		print_error(vars, NULL);
+		vars->status = 127;
+		new_exit(vars);
+	}
+	for (i = 0; vars->env[i] != *key; i++)
+		newenv[i] = vars->env[i];
+	for (j = i + 1; vars->env[j] != NULL; j++, i++)
+		newenv[i] = vars->env[j];
+	newenv[i] = NULL;
+	free(*key);
+	free(vars->env);
+	vars->env = newenv;
+	vars->status = 0;
 }
